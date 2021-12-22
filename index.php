e8982c68e87c9a92cbee10edd6e7503a898996f6
ashu <ashu20779@gmail.com>
2021-12-22 16:56:58 +0530

    update files

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include("conn.php");
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>
     </title>
 </head>
